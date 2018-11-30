d3a98a0beb3a66cd0bac2fe7efc736aa6b47fd5f
Charley McGowan and Scott Bergler <charley@>
2018-11-29 16:48:36 -0800

    Final commit of the day, got index view to index test to work

--- MusicOrganizer.Tests/ControllerTests/ArtistControllerTests.cs
+++ MusicOrganizer.Tests/ControllerTests/ArtistControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using MusicOrganizer.Controllers;
 using MusicOrganizer.Models;
+using System;
 
 namespace MusicOrganizer.Tests
 {
@@ -10,7 +11,7 @@ namespace MusicOrganizer.Tests
   public class ArtistControllerTest
   {
     [TestMethod]
-    public void Index_ReturnsCorrectView_True()
+    public void Index_ReturnsAViewResult_True()
     {
         //Arrange
       ArtistController controller = new ArtistController();
@@ -23,10 +24,23 @@ namespace MusicOrganizer.Tests
     }
 
     [TestMethod]
+    public void Index_RedirectsToCorrectAction_Index()
+    {
+        //Arrange
+        ArtistController controller = new ArtistController();
+        var viewResult = controller.Index() as ViewResult;
+
+        //Act
+        var result = viewResult.ViewName;
+        // Console.WriteLine("This is viewResult.Viewname: " + viewResult.ViewName);
+        //Assert
+        Assert.AreEqual(result, "Index");
+    }
+
+    [TestMethod]
     public void Index_HasCorrectModelType_ArtistsList()
     {
         //Arrange
-        // ArtistController controller = new ArtistController();
         ViewResult indexView = new ArtistController().Index() as ViewResult;
 
         //Act
@@ -34,6 +48,44 @@ namespace MusicOrganizer.Tests
 
         //Assert
         Assert.IsInstanceOfType(result, typeof(List<Artist>));
+    }
+
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    {
+      ArtistController controller = new ArtistController();
+
+      ActionResult createView = controller.Create("QOTSA");
+
+      Assert.IsInstanceOfType(createView, typeof(RedirectToActionResult));
+    }
+
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index()
+    {
+        //Arrange
+        ArtistController controller = new ArtistController();
+        RedirectToActionResult actionResult = controller.Create("Walk the dog") as RedirectToActionResult;
+
+        //Act
+        string result = actionResult.ActionName;
+
+        //Assert
+        Assert.AreEqual(result, "Index");
+    }
+
+    [TestMethod]
+    public void Create_HasCorrectModelType_ArtistObject()
+    {
+        //Arrange
+        ArtistController createView = new ArtistController();
+        ViewResult createObject = createView.Create("QOTSA") as ViewResult;
+
+        //Act
+        var result = createView.ViewData.Model;
+
+        //Assert
+        Assert.IsInstanceOfType(result, typeof(Artist));
     }
   }
 }
--- MusicOrganizer/Controllers/ArtistsController.cs
+++ MusicOrganizer/Controllers/ArtistsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MusicOrganizer.Models;
+using System.Collections.Generic;
 
 namespace MusicOrganizer.Controllers
 {
@@ -8,9 +9,16 @@ namespace MusicOrganizer.Controllers
         [HttpGet("/artists")]
         public ActionResult Index()
         {
-          return View();
+          List<Artist> allArtists = Artist.GetAll();
+          ViewResult viewResult = View("Index", allArtists);
+          return viewResult;
         }
 
-
+        [HttpPost("/artists")]
+        public ActionResult Create(string artistName)
+        {
+          //Artist newArtist = new Artist(artistName);
+          return RedirectToAction("Index");
+        }
     }
 }
